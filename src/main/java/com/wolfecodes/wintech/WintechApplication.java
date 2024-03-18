2ee45db6f0e5e4c6775c1ced611a2b71efafecb5
unknown <marc.wolfe21@gmail.com>
2024-03-18 12:59:41 -0500

    Initial commit

---
+++ src/main/java/com/wolfecodes/wintech/WintechApplication.java
@@ -0,0 +1,13 @@
+package com.wolfecodes.wintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wolfecodes/wintech/WintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolfecodes.wintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
